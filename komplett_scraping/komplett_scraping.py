8ad4e09df6b414b25fd890dbf36011497bddb388
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-07-05 13:22:25 +0200

    Add "default" class for Scraper (change name) and added class for Proshop scraping
    Made class Komplett and class Proshop inherent from class Scraper.
    Now tracks 2080TI from Proshop also

--- komplett_scraping/komplett_scraping.py
+++ komplett_scraping/komplett_scraping.py
@@ -24,7 +24,7 @@ def log_setup():
     return logger
 
 
-class Komplett:
+class Scraper:
     def __init__(self, category, URL):
         logger.debug(f'Initiating instance of class "{self.__class__.__name__}"')
         self.cat = category
@@ -42,18 +42,11 @@ class Komplett:
         except Exception as err:
             logger.error(f'Failed in method "{self.__class__.__name__}.save_record()": {err}', exc_info=True)
 
-    def get_info(self):
-        logger.info('Getting response from URL...')
-        self.response = requests.get(self.URL)
-        logger.info('Got response from URL')
-        self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
-        self.name = self.html_soup.find_all('div', class_='product-main-info__info')[0].h1.span.text
-        try:
-            self.price = ''.join(self.html_soup.find_all('div', class_='price-freight')[0].div.span.text.strip('.,-').split('.'))
-        except AttributeError:
-            self.price = ''.join(self.html_soup.find_all('div', class_='price-freight')[0].span.text.strip('.,-').split('.'))
-        self.part_num = self.URL.split('/')[4]
-        self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
+    def get_info(self): # gets overwritten
+        self.name = ''
+        self.price = ''
+        self.date = ''
+        self.part_num = ''
 
     def print_info(self):
         print(f'Kategori: {self.cat}\nNavn: {self.name}\nPris: {self.price} kr.\nDato: {self.date}\nFra domain: {self.URL_domain}\nProdukt nummer: {self.part_num}\n')
@@ -63,11 +56,42 @@ class Komplett:
         logger.info('Saving record...')
         with open('records.json', 'r') as json_file:
             data = json.load(json_file)
-            data[self.cat][self.date] = {"name": f"{self.name}", "price": f"{self.price}", "from": f'{self.URL_domain}', "part_num": f'{self.part_num}'}
+            #data[self.cat][self.date] = {"name": f"{self.name}", "price": f"{self.price}", "from": f'{self.URL_domain}', "part_num": f'{self.part_num}'}
+            data[self.cat][self.name][self.URL_domain]["dates"][self.date] = {"price": f"{self.price}"}
         with open('records.json', 'w') as json_file:
-            json.dump(data, json_file, indent=2)
+            json.dump(data, json_file, indent=4)
+            #json.dump(data, json_file)
         #print('Record saved')
         logger.info('Record saved')
+
+
+class Komplett(Scraper):
+    def get_info(self):
+        logger.info('Getting response from URL...')
+        self.response = requests.get(self.URL)
+        logger.info('Got response from URL')
+        self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
+        self.name = self.html_soup.find_all('div', class_='product-main-info__info')[0].h1.span.text.lower()
+        try:
+            self.price = ''.join(self.html_soup.find_all('div', class_='price-freight')[0].div.span.text.strip('.,-').split('.'))
+        except AttributeError:
+            self.price = ''.join(self.html_soup.find_all('div', class_='price-freight')[0].span.text.strip('.,-').split('.'))
+        self.part_num = self.URL.split('/')[4]
+        self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
+
+
+class Proshop(Scraper):
+    def get_info(self):
+        logger.info('Getting response from URL...')
+        self.response = requests.get(self.URL)
+        logger.info('Got response from URl')
+        self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
+        self.name = self.html_soup.find_all('div', class_='col-xs-12 col-sm-7')[0].h1.text.lower()
+        if "asus geforce rtx 2080 ti rog strix oc" in self.name:
+            self.name = "asus geforce rtx 2080 ti rog strix oc"
+        self.price = ''.join(self.html_soup.find_all('div', class_='col-md-7')[0].span.span.text.strip('.-kr').split(',')[0].split('.'))
+        self.part_num = self.html_soup.find('small', class_='col-xs-12 text-center').strong.text
+        self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
 
 
 def multiple_links(file_name):
@@ -84,5 +108,6 @@ def multiple_links(file_name):
 
 if __name__ == '__main__':
     logger = log_setup()
-    gpu = Komplett('gpu', 'https://www.komplett.dk/product/1103205/hardware/pc-komponenter/grafikkort/asus-geforce-rtx-2080-ti-rog-strix-oc#')
-    ssd = Komplett('ssd', 'https://www.komplett.dk/product/1133452/hardware/lagring/harddiskssd/ssd-m2/corsair-force-series-mp600-1tb-m2-ssd#')
+    Komplett('gpu', 'https://www.komplett.dk/product/1103205/hardware/pc-komponenter/grafikkort/asus-geforce-rtx-2080-ti-rog-strix-oc#')
+    Komplett('ssd', 'https://www.komplett.dk/product/1133452/hardware/lagring/harddiskssd/ssd-m2/corsair-force-series-mp600-1tb-m2-ssd#')
+    Proshop('gpu', 'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-2080-Ti-ROG-STRIX-OC-11GB-GDDR6-RAM-Grafikkort/2679518')
