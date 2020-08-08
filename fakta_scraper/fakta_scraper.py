1a8a82608867d68dd3d555be380add83fd736cd9
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-08-08 21:58:00 +0200

    Remove unnecessary comments

--- fakta_scraper/fakta_scraper.py
+++ fakta_scraper/fakta_scraper.py
@@ -25,18 +25,13 @@ driver.get(URL)
 accept_cookies_button = driver.find_element_by_id('acceptAllButton')
 accept_cookies_button.send_keys(Keys.RETURN)
 
-
-# wait.until(presence_of_element_located((By.CLASS_NAME, 'incito__view incito__absolute-layout-view')))
 wait.until(presence_of_element_located((By.XPATH, '/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div/div[1]')))
 
 print(driver.title)
 print(driver.current_url)
 
-#products = driver.find_elements_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div/div')
-x = 2
-#products = driver.find_elements_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div')
-products = driver.find_elements_by_xpath(f'/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div')
 
+products = driver.find_elements_by_xpath(f'/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div')
 
 
 word_tilbud = ['lays', 'majskolber', 'kellogg']
@@ -49,22 +44,6 @@ for product in products:
     for word in word_tilbud:
         if word in product.text.lower():
             tilbud_søgning.append(product.text)
-
-
-    # if 'lays' in product.text.lower():
-    #     test = True
-    #     print('\nHey der er Lays på tilbud\n\n\n\n\n\n')
-    #     tilbud_søgning.append(product.text)
-
-
-# for x in range(2, 26):
-#     #print("hello")
-#     products = driver.find_elements_by_xpath(f'/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div{x}/div/div')
-
-#     #print(f'{products}\n\n')
-
-#     for product in products:
-#         print(f'{product.text}\n')
 
 
 tidsperiode = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div[1]/div/p')
