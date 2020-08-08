ba6a5b3608858a849c646289d0679843a2b18ad3
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-08-09 00:11:18 +0200

    Trying to seperate each product instead of it being in groups

--- fakta_scraper/fakta_scraper.py
+++ fakta_scraper/fakta_scraper.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,14 +7,10 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 
 
 # Setup headless browser
-# firefox_options = Options()
-# firefox_options.add_argument('--headless')
-
-# driver = webdriver.Firefox(
-#     options=firefox_options
-#     )
-
-driver = webdriver.Firefox()
+firefox_options = Options()
+firefox_options.add_argument('--headless')
+driver = webdriver.Firefox(options=firefox_options)
+# driver = webdriver.Firefox()
 
 URL = 'https://fakta.coop.dk/tilbudsavis/'
 
@@ -38,7 +33,7 @@ print(driver.current_url)
 # Find all the products on discount
 products = driver.find_elements_by_xpath(f'/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div')
 
-# Discount to look for
+# Discounts to look for
 discount_words = ['lays', 'majskolber', 'kellogg']
 
 found_discounts = []
@@ -54,10 +49,52 @@ for product in products:
 time_periode = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div[1]/div/p')
 print(time_periode.text)
 
+#num = 0
+#new_list = []
+#for product in found_discounts:
+#    new_word = ''
+#    for letter in product:
+#        if letter == 'n':
+#            if last_letter == '\\':
+#                if num%2 == 0:
+#                    letter = ' '
+#        if letter == '\\':
+#            if num%2 == 0:
+#                letter = ''
+#            num += 1
+#        last_letter = letter
+#        new_word += letter
+#    new_list.append(new_word)
+
+
+new_list = []
+for product in found_discounts:
+    indi_product = product.split('\n')
+    for indi in indi_product:
+        new_list.append(indi)
+
+num = 0
+new_list_1 = []
+for product in new_list:
+    if num%2 != 0:
+        new_list_1.append(product)
+    if num != 1:
+        if num%2 == 0:
+            num_1 = int(num/2)
+            print(new_list_1[num_1])
+            new_list_1[num_1] += ' '+product
+    num += 1
+
+
+
 # Print found discounts
 print('\n\n\nTilbud:')
 for discount in found_discounts:
     print(f'{discount}\n')
+
+print(found_discounts)
+
+print(new_list)
 
 # input("enter to quit")
 driver.quit()
