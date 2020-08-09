1c27e586fc6a51c7c7a406825d730311e286c507
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-08-09 18:43:52 +0200

    Import sys to implement arguments

--- fakta_scraper/fakta_scraper.py
+++ fakta_scraper/fakta_scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
+import sys
 
 
 def main():
