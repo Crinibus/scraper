abf7e0d0896a36632f57ea1fb9035171601c9e4c
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-08-09 15:19:50 +0200

    Clean up the code and add comments

--- fakta_scraper/fakta_scraper.py
+++ fakta_scraper/fakta_scraper.py
@@ -31,54 +31,35 @@ print(driver.title)
 print(driver.current_url)
 
 # Find all the products on discount
-products = driver.find_elements_by_xpath(f'/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div')
+products = driver.find_elements_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div')
+
 
 # Discounts to look for
 discount_words = ['lays', 'majskolber', 'kellogg']
 
-found_discounts = []
-
 # Find the products that match the ones to look for
+found_discounts = []
 for product in products:
     # print(f'{product.text}\n')
     for word in discount_words:
         if word in product.text.lower():
             found_discounts.append(product.text)
 
+
 # Find the time period the discounts is valid for
 time_periode = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div[2]/div/div[1]/div/p')
 print(time_periode.text)
 
 
-
-
-
-#num = 0
-#new_list = []
-#for product in found_discounts:
-#    new_word = ''
-#    for letter in product:
-#        if letter == 'n':
-#            if last_letter == '\\':
-#                if num%2 == 0:
-#                    letter = ' '
-#        if letter == '\\':
-#            if num%2 == 0:
-#                letter = ''
-#            num += 1
-#        last_letter = letter
-#        new_word += letter
-#    new_list.append(new_word)
-
-
+# Seperate products and prices
 new_list = []
 for product in found_discounts:
     indi_product = product.split('\n')
     for indi in indi_product:
         new_list.append(indi)
 
-print(new_list)
 
+# Combine a product with it's price
 num = 0
 seperate_products = []
 for product in new_list:
@@ -88,19 +69,18 @@ for product in new_list:
     else:
         seperate_products[-1] += ' '+product
 
+# Get rid of duplicates
+check_products = []
+for product in seperate_products:
+    if not product in check_products:
+        check_products.append(product)
 
 
-
-
-# Print found discounts
-print('\n\n\nTilbud:')
-for discount in found_discounts:
+# Print discounts
+print('\nTilbud:')
+for discount in check_products:
     print(f'{discount}\n')
 
-print(found_discounts)
-
-for discount in seperate_products:
-    print(discount)
 
 # input("enter to quit")
 driver.quit()
