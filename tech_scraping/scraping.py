70e61d67477917af5cf0ece9ce0a26d2e36a95f3
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-08-19 12:02:47 +0200

    Add class "AvXperten"

--- tech_scraping/scraping.py
+++ tech_scraping/scraping.py
@@ -168,7 +168,6 @@ class Komplett(Scraper):
     def get_info(self):
         self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
         self.name = self.html_soup.find('div', class_='product-main-info__info').h1.span.text.lower()
-        # find price
         self.price = self.html_soup.find('span', class_='product-price-now').text.strip(',-').replace('.', '')
         self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
 
@@ -194,7 +193,6 @@ class Computersalg(Scraper):
     def get_info(self):
         self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
         self.name = self.html_soup.find('h1', itemprop='name').text.lower()
-        # find price
         self.price = self.html_soup.find('span', itemprop='price').text.strip().split(',')[0].replace('.', '')
         self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
 
@@ -203,8 +201,15 @@ class Elgiganten(Scraper):
     def get_info(self):
         self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
         self.name = self.html_soup.find('h1', class_='product-title').text.lower()
-        # find price
         self.price = self.html_soup.find('div', class_='product-price-container').text.strip().replace(u'\xa0', '')
+        self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
+
+
+class AvXperten(Scraper):
+    def get_info(self):
+        self.html_soup = BeautifulSoup(self.response.text, 'html.parser')
+        self.name = self.html_soup.find('div', class_='content-head').text.strip().lower()
+        self.price = self.html_soup.find('div', class_='price').text.replace(u'\xa0DKK', '')
         self.date = str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
 
 
