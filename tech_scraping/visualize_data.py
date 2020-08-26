9c5febe96c482f4d9dd67c13e7fb0e95cdf87f71
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-08-27 00:34:30 +0200

    Seperate code into more functions

--- tech_scraping/visualize_data.py
+++ tech_scraping/visualize_data.py
@@ -1,41 +1,49 @@
 import json
 import matplotlib.pyplot as plt
 
-
-with open('records.json', 'r') as jsonfile:
-    data = json.load(jsonfile)
-
-# Get dates
-komplett_dates = [date for date in data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.komplett.dk']['dates']]
-proshop_dates = [date for date in data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.proshop.dk']['dates']]
-
-# Get prices
-komplett_prices = [int(data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.komplett.dk']['dates'][date]['price']) for date in komplett_dates]
-proshop_prices = [int(data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.proshop.dk']['dates'][date]['price']) for date in proshop_dates]
-
-# komplett_num_days = [i for i in range(len(komplett_dates))]
-# proshop_num_days = [i for i in range(len(proshop_dates))]
+def read_records():
+    with open('records.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    return data
 
 
-# Plotting
-plt.plot(komplett_dates, komplett_prices, 
-         proshop_dates, proshop_prices, 
-         marker='o', linestyle='-')
+def asus_2080ti():
 
-# Styling
-plt.style.use('seaborn-darkgrid')
-plt.title('Prices of ASUS 2080 TI ROG Strix')
-plt.xlabel('Day')
-plt.ylabel('Price')
-plt.xticks(rotation=70)
-plt.legend(['Komplett', 'Proshop'])
+    data = read_records()
 
-# Show graph
-plt.show()
+    # Get dates
+    komplett_dates = [date for date in data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.komplett.dk']['dates']]
+    proshop_dates = [date for date in data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.proshop.dk']['dates']]
 
+    # Get prices
+    komplett_prices = [int(data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.komplett.dk']['dates'][date]['price']) for date in komplett_dates]
+    proshop_prices = [int(data['gpu']['asus geforce rtx 2080 ti rog strix oc']['www.proshop.dk']['dates'][date]['price']) for date in proshop_dates]
+
+    # komplett_num_days = [i for i in range(len(komplett_dates))]
+    # proshop_num_days = [i for i in range(len(proshop_dates))]
+
+
+    # Plotting
+    plt.plot(komplett_dates, komplett_prices, 
+            proshop_dates, proshop_prices, 
+            marker='o', linestyle='-')
+
+    # Styling
+    plt.style.use('seaborn-darkgrid')
+    plt.title('Prices of ASUS 2080 TI ROG Strix')
+    plt.xlabel('Day')
+    plt.ylabel('Price')
+    plt.xticks(rotation=70)
+    plt.legend(['Komplett', 'Proshop'])
+
+    # Show graph
+    plt.show()
 
 
 def show_all():
+    
+    data = read_records()
+
     for category in data:
         for product in data[category]:
             dates_1 = []
