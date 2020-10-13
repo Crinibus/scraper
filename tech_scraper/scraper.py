bec37b40e675f1bbea8d929173c5ac711a114933
Crinibus <57172157+Crinibus@users.noreply.github.com>
2020-10-13 21:09:49 +0200

    Change how conditions is in function change_name

--- tech_scraper/scraper.py
+++ tech_scraper/scraper.py
@@ -215,11 +215,11 @@ def change_name(name):
     Change the name of the product, so if a similiar product is also
     being scraped, the similar products goes under the same name.
     """
-    if 'asus' in name and 'rtx' in name and '2080' in name and 'ti' in name and 'rog' in name and 'strix' in name:
+    if all(['asus', 'rtx', '2080', 'ti', 'rog', 'strix']) in name:
         name = 'asus geforce rtx 2080 ti rog strix oc'
-    elif 'asus' in name and 'rtx' in name and '3080' in name and 'rog' in name and 'strix' in name and 'oc' in name:
+    elif all(['asus', 'rtx', '3080', 'rog', 'strix', 'oc']) in name:
         name = 'asus geforce rtx 3080 rog strix oc'
-    elif 'corsair' in name and 'mp600' in name and '1tb' in name and 'm.2' in name:
+    elif all(['corsair', 'mp600', '1tb', 'm.2']) in name:
         name = 'corsair force mp600 1tb m.2'
     return name
 
