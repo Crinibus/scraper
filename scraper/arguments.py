1f19227f2bb1b98e16a2521c863dbf3ed9ad59a1
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-03-30 18:50:35 +0200

    Update arguments.py
    Add argument --scrape

--- scraper/arguments.py
+++ scraper/arguments.py
@@ -6,6 +6,10 @@ def argparse_setup() -> ArgumentParser.parse_args:
     parser = ArgumentParser()
 
     parser.add_argument(
+        "-s", "--scrape", help="scrape product prices", action="store_true",
+    )
+
+    parser.add_argument(
         "-c", "--category", help="the category the product is going to be in", type=str
     )
 
@@ -13,91 +17,91 @@ def argparse_setup() -> ArgumentParser.parse_args:
 
     parser.add_argument(
         "--komplett",
-        help="add only komplett-domain under the product-name,"
+        help="add only komplett-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--proshop",
-        help="add only proshop-domain under the product-name,"
+        help="add only proshop-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--computersalg",
-        help="add only computersalg-domain under the product-name,"
+        help="add only computersalg-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--elgiganten",
-        help="add only elgiganten-domain under the product-name,"
+        help="add only elgiganten-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--avxperten",
-        help="add only avxperten-domain under the product-name,"
+        help="add only avxperten-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--avcables",
-        help="add only avcables-domain under the product-name,"
+        help="add only avcables-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--amazon",
-        help="add only amazon-domain under the product-name,"
+        help="add only amazon-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--ebay",
-        help="add only ebay-domain under the product-name,"
+        help="add only ebay-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--power",
-        help="add only power-domain under the product-name,"
+        help="add only power-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--expert",
-        help="add only expert-domain under the product-name,"
+        help="add only expert-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--mmvision",
-        help="add only mm-vision-domain under the product-name,"
+        help="add only mm-vision-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--coolshop",
-        help="add only coolshop-domain under the product-name,"
+        help="add only coolshop-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
 
     parser.add_argument(
         "--sharkgaming",
-        help="add only sharkgaming-domain under the product-name,"
+        help="add only sharkgaming-domain under the product-name, "
         "if this is the only optional flag",
         action="store_true",
     )
