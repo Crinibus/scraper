6a5d41d4aad72de6a3c2da51f64b31491b2dd572
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-03-31 13:51:36 +0200

    Update add_product.py
    Add function "add_product"

---
+++ scraper/add_product.py
@@ -0,0 +1,23 @@
+from scraper.scrape import Scraper
+from scraper.filemanager import Filemanager
+
+
+def add_product(args):
+    print("Adding product...")
+
+    new_product = Scraper(args.category, args.url)
+    new_product.scrape_info()
+
+    data = Filemanager.get_record_data()
+
+    data.update(
+        {
+            args.category: {
+                new_product.info.name: {
+                    new_product.website_name: {"info": {}, "dates": {}}
+                }
+            }
+        }
+    )
+
+    new_product.save_info()
