c13490130027a895f1f6147ce2eabfd4f7187f2c
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-04-07 20:38:02 +0200

    Update __init__.py
    Import show functions from visualize.py

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -2,3 +2,4 @@ from .scrape import Scraper
 from .arguments import argparse_setup
 from .add_product import add_product
 from .filemanager import Filemanager
+from .visualize import show_id, show_category, show_all_products
