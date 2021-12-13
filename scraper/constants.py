632d1b4b950dfc3c1c7062e2d5ac002a28529631
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-12-13 23:33:01 +0100

    Update REQUEST_HEADER to newer version of Chrome
    Split line with string to REQUEST_HEADER up into two lines

--- scraper/constants.py
+++ scraper/constants.py
@@ -1,5 +1,6 @@
 REQUEST_HEADER = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
 }
 
 REQUEST_COOKIES = {"cookies_are": "working"}
