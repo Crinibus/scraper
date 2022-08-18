000a0b3598e6d5f2bd52e56c6919d1f53f4b8910
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-08-18 22:49:01 +0200

    Add test classes for missing website handlers

--- tests/test_website_handlers.py
+++ tests/test_website_handlers.py
@@ -3,10 +3,16 @@ from scraper.domains import (
     AvCablesHandler,
     AvXpertenHandler,
     ComputerSalgHandler,
+    CoolshopHandler,
     EbayHandler,
     ElgigantenHandler,
+    ExpertHandler,
     KomplettHandler,
+    MMVisionHandler,
+    NeweggHandler,
+    PowerHandler,
     ProshopHandler,
+    SharkGamingHandler,
 )
 from scraper.format import Info
 
@@ -57,6 +63,30 @@ amazon_handler._get_common_data(amazon_soup)
 # ebay_soup_two = ebay_handler_two._request_product_data()
 # ebay_handler_two._get_common_data(ebay_soup_two)
 
+expert_handler = ExpertHandler("https://www.expert.dk/hoejtalere-og-lyd/hovedtelefoner/traadloese-hovedtelefoner/sony-wh-1000xm4-traadloese-stoejdaempende-hovedtelefoner-sort/p-1106907/")
+expert_soup = expert_handler._request_product_data()
+expert_handler._get_common_data(expert_soup)
+
+power_handler = PowerHandler("https://www.power.dk/tv-og-lyd/hovedtelefoner/traadloese-hovedtelefoner/sony-wh-1000xm4-traadloese-stoejdaempende-hovedtelefoner-blaa/p-1185731/")
+power_soup = power_handler._request_product_data()
+power_handler._get_common_data(power_soup)
+
+mmvision_handler = MMVisionHandler("https://www.mm-vision.dk/asus-zenbook-duo-14-ux482eg-pure9x-baerbar")
+mmvision_soup = mmvision_handler._request_product_data()
+mmvision_handler._get_common_data(mmvision_soup)
+
+coolshop_handler = CoolshopHandler("https://www.coolshop.dk/produkt/pokemon-brilliant-diamond/238G6U/")
+coolshop_soup = coolshop_handler._request_product_data()
+coolshop_handler._get_common_data(coolshop_soup)
+
+sharkgaming_handler = SharkGamingHandler("https://sharkgaming.dk/asus-gladius-ii-origin-gaming-mouse")
+sharkgaming_soup = sharkgaming_handler._request_product_data()
+sharkgaming_handler._get_common_data(sharkgaming_soup)
+
+newegg_handler = NeweggHandler("https://www.newegg.com/sony-wh1000xm4b-bluetooth-headset-black/p/0G6-001C-00614?Description=sony%20xm4&cm_re=sony_xm4-_-0G6-001C-00614-_-Product&quicklink=true")
+newegg_soup = newegg_handler._request_product_data()
+newegg_handler._get_common_data(newegg_soup)
+
 
 class TestKomplettHandler:
     def test_get_product_info(self):
@@ -65,7 +95,7 @@ class TestKomplettHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = komplett_handler._get_product_name(komplett_soup)
+        actual = komplett_handler._get_product_name(komplett_soup).lower()
         expected = "ASUS GeForce RTX 3090 ROG Strix OC".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -92,7 +122,7 @@ class TestProshopHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = proshop_handler._get_product_name(proshop_soup)
+        actual = proshop_handler._get_product_name(proshop_soup).lower()
         expected = "Sony WH-1000XM4".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -119,7 +149,7 @@ class TestComputersalgHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = computersalg_handler._get_product_name(computersalg_soup)
+        actual = computersalg_handler._get_product_name(computersalg_soup).lower()
         expected = "Sony WH-1000XM4 - Hovedtelefoner med mik. - fuld størrelse - Bluetooth - trådløs, kabling - NFC - aktiv støjfjerning - 3,5 mm jackstik - sort".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -146,7 +176,7 @@ class TestElgigantenHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = elgiganten_handler._get_product_name(elgiganten_soup)
+        actual = elgiganten_handler._get_product_name(elgiganten_soup).lower()
         expected = "Sony trådløse around-ear høretelefoner WH-1000XM4 (sort)".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -173,7 +203,7 @@ class TestAvXpertenHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = avxperten_handler._get_product_name(avxperten_soup)
+        actual = avxperten_handler._get_product_name(avxperten_soup).lower()
         expected = "Sony WH-1000XM4 Bluetooth hovedtelefoner (m/ANC) Sort".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -200,7 +230,7 @@ class TestAvCablesHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = avcables_handler._get_product_name(avcables_soup)
+        actual = avcables_handler._get_product_name(avcables_soup).lower()
         expected = "Sony WH-1000XM4 Over-Ear Bluetooth Headset - Sort".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -227,7 +257,7 @@ class TestAmazonHandler:
         assert actual.valid == True
 
     def test_get_name(self):
-        actual = amazon_handler._get_product_name(amazon_soup)
+        actual = amazon_handler._get_product_name(amazon_soup).lower()
         expected = "Sony WH-1000XM4 Wireless Noise Canceling Overhead Headphones - Black (Renewed)".lower()
         assert isinstance(actual, str)
         assert actual == expected
@@ -247,7 +277,7 @@ class TestAmazonHandler:
         assert id == "B08HDKHSSN"
 
 
-# # OBS: There is two Ebay versions
+# # OBS: There is two Ebay versions - WITH 'itm' IN URL
 # class TestEbayHandlerVersionOne:
 #     def test_get_product_info(self):
 #         actual = ebay_handler_one.get_product_info()
@@ -255,7 +285,7 @@ class TestAmazonHandler:
 #         assert actual.valid == True
 
 #     def test_get_name(self):
-#         actual = ebay_handler_one._get_product_name(ebay_soup_one)
+#         actual = ebay_handler_one._get_product_name(ebay_soup_one).lower()
 #         expected = "SET TITLE".lower()
 #         assert isinstance(actual, str)
 #         assert actual == expected
@@ -275,7 +305,7 @@ class TestAmazonHandler:
 #         assert id == "SET ID"
 
 
-# # OBS: There is two Ebay versions
+# # OBS: There is two Ebay versions - WITHOUT 'itm' IN URL
 # class TestEbayHandlerVersionTwo:
 #     def test_get_product_info(self):
 #         actual = ebay_handler_two.get_product_info()
@@ -283,7 +313,7 @@ class TestAmazonHandler:
 #         assert actual.valid == True
 
 #     def test_get_name(self):
-#         actual = ebay_handler_two._get_product_name(ebay_soup_two)
+#         actual = ebay_handler_two._get_product_name(ebay_soup_two).lower()
 #         expected = "SET TITLE".lower()
 #         assert isinstance(actual, str)
 #         assert actual == expected
@@ -304,24 +334,162 @@ class TestAmazonHandler:
 
 
 class TestPowerHandler:
-    pass
+    def test_get_product_info(self):
+        actual = power_handler.get_product_info()
+        assert isinstance(actual, Info)
+        assert actual.valid == True
+
+    def test_get_name(self):
+        actual = power_handler._get_product_name(power_soup).lower()
+        expected = "SONY WH-1000XM4 TRÅDLØSE STØJDÆMPENDE HOVEDTELEFONER BLÅ".lower()
+        assert isinstance(actual, str)
+        assert actual == expected
+
+    def test_get_price(self):
+        price = power_handler._get_product_price(power_soup)
+        assert isinstance(price, float)
+
+    def test_get_currency(self):
+        currency = power_handler._get_product_currency(power_soup)
+        assert isinstance(currency, str)
+        assert currency == "DKK"
+
+    def test_get_id(self):
+        id = power_handler._get_product_id(power_soup)
+        assert isinstance(id, str)
+        assert id == "1185731"
 
 
 class TestExpertHandler:
-    pass
+    def test_get_product_info(self):
+        actual = expert_handler.get_product_info()
+        assert isinstance(actual, Info)
+        assert actual.valid == True
+
+    def test_get_name(self):
+        actual = expert_handler._get_product_name(expert_soup).lower()
+        expected = "SONY WH-1000XM4 TRÅDLØSE STØJDÆMPENDE HOVEDTELEFONER SORT".lower()
+        assert isinstance(actual, str)
+        assert actual == expected
+
+    def test_get_price(self):
+        price = expert_handler._get_product_price(expert_soup)
+        assert isinstance(price, float)
+
+    def test_get_currency(self):
+        currency = expert_handler._get_product_currency(expert_soup)
+        assert isinstance(currency, str)
+        assert currency == "DKK"
+
+    def test_get_id(self):
+        id = expert_handler._get_product_id(expert_soup)
+        assert isinstance(id, str)
+        assert id == "1106907"
 
 
 class TestMMVisionHandler:
-    pass
+    def test_get_product_info(self):
+        actual = mmvision_handler.get_product_info()
+        assert isinstance(actual, Info)
+        assert actual.valid == True
+
+    def test_get_name(self):
+        actual = mmvision_handler._get_product_name(mmvision_soup).lower()
+        expected = "Asus ZenBook Duo 14 UX482".lower()
+        assert isinstance(actual, str)
+        assert actual == expected
+
+    def test_get_price(self):
+        price = mmvision_handler._get_product_price(mmvision_soup)
+        assert isinstance(price, float)
+
+    def test_get_currency(self):
+        currency = mmvision_handler._get_product_currency(mmvision_soup)
+        assert isinstance(currency, str)
+        assert currency == "DKK"
+
+    def test_get_id(self):
+        id = mmvision_handler._get_product_id(mmvision_soup)
+        assert isinstance(id, str)
+        assert id == "6987132"
 
 
 class TestCoolshopHandler:
-    pass
+    def test_get_product_info(self):
+        actual = coolshop_handler.get_product_info()
+        assert isinstance(actual, Info)
+        assert actual.valid == True
+
+    def test_get_name(self):
+        actual = coolshop_handler._get_product_name(coolshop_soup).lower()
+        expected = "Pokemon Brilliant Diamond - Nintendo Switch".lower()
+        assert isinstance(actual, str)
+        assert actual == expected
+
+    def test_get_price(self):
+        price = coolshop_handler._get_product_price(coolshop_soup)
+        assert isinstance(price, float)
+
+    def test_get_currency(self):
+        currency = coolshop_handler._get_product_currency(coolshop_soup)
+        assert isinstance(currency, str)
+        assert currency == "DKK"
+
+    def test_get_id(self):
+        id = coolshop_handler._get_product_id(coolshop_soup)
+        assert isinstance(id, str)
+        assert id == "1177871"
 
 
 class TestSharkGamingHandler:
-    pass
+    def test_get_product_info(self):
+        actual = sharkgaming_handler.get_product_info()
+        assert isinstance(actual, Info)
+        assert actual.valid == True
+
+    def test_get_name(self):
+        actual = sharkgaming_handler._get_product_name(sharkgaming_soup).lower()
+        expected = "ASUS Gladius II Origin gaming mouse".lower()
+        assert isinstance(actual, str)
+        assert actual == expected
+
+    def test_get_price(self):
+        price = sharkgaming_handler._get_product_price(sharkgaming_soup)
+        assert isinstance(price, float)
+
+    def test_get_currency(self):
+        currency = sharkgaming_handler._get_product_currency(sharkgaming_soup)
+        assert isinstance(currency, str)
+        assert currency == "DKK"
+
+    def test_get_id(self):
+        id = sharkgaming_handler._get_product_id(sharkgaming_soup)
+        assert isinstance(id, str)
+        assert id == "90MP00U1-B0UA00"
 
 
 class TestNeweggHandler:
-    pass
+    def test_get_product_info(self):
+        actual = newegg_handler.get_product_info()
+        assert isinstance(actual, Info)
+        assert actual.valid == True
+
+    def test_get_name(self):
+        actual = newegg_handler._get_product_name(newegg_soup).lower()
+        expected = "Sony WH-1000XM4 Wireless Noise-Cancelling Over-Ear Headphones (Black)".lower()
+        assert isinstance(actual, str)
+        assert actual == expected
+
+    def test_get_price(self):
+        price = newegg_handler._get_product_price(newegg_soup)
+        assert isinstance(price, float)
+
+    def test_get_currency(self):
+        currency = newegg_handler._get_product_currency(newegg_soup)
+        assert isinstance(currency, str)
+        assert currency == "USD"
+
+    def test_get_id(self):
+        id = newegg_handler._get_product_id(newegg_soup)
+        assert isinstance(id, str)
+        assert id == "0G6-001C-00614"
