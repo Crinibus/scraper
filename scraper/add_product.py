952a5b3120c5fcd5f38567c6944f620f7d9616f1
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-08-24 17:44:02 +0200

    Move print and logging "Adding product..." to after check if website is supported

--- scraper/add_product.py
+++ scraper/add_product.py
@@ -12,9 +12,7 @@ def add_products(categories: List[str], urls: List[str]):
 
 
 def add_product(category: str, url: str) -> None:
-    print("Adding product...")
     logger = logging.getLogger(__name__)
-    logger.info(f"Adding product: {category} - {url}")
 
     website_name = get_website_name(url)
 
@@ -22,6 +20,9 @@ def add_product(category: str, url: str) -> None:
         print(f"Can't scrape from this website: {website_name}")
         logger.info(f"Not supported website to scrape from: {website_name}")
         raise WebsiteNotSupported(f"'{website_name}' is currently not supported")
+
+    print(f"Adding product with category '{category}' and url '{url[0:min(50, len(url))]}'...")
+    logger.info(f"Adding product with category '{category}' and url '{url}'")
 
     new_product = Scraper(category, url)
     new_product.scrape_info()
