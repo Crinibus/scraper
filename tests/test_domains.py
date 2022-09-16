713df955afd013cad78be613e88d6c5d79fa9364
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-09-16 07:51:23 +0200

    Add tests for function 'get_website_name'

---
+++ tests/test_domains.py
@@ -0,0 +1,19 @@
+import pytest
+
+from scraper.domains import get_website_name
+
+test_websites = [
+    ("https://www.amazon.com/", "amazon"),
+    ("https://www.komplett.dk/", "komplett"),
+    ("https://www.av-cables.dk/", "av-cables"),
+    ("https://nowww.com/", "nowww"),
+    ("https://no-ending-slash.com", "no-ending-slash"),
+    ("https://www.test.testing.com/", "test.testing"),
+    ("https://www.test.hello.com/hello/world", "test.hello"),
+]
+
+
+@pytest.mark.parametrize("url,expected", test_websites)
+def test_get_website_name(url, expected):
+    result = get_website_name(url)
+    assert result == expected
