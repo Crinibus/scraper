e85851e7ab2636a19492e6f11d977579624939c3
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-10-11 23:37:36 +0200

    Delete rows in products.csv that match with the products that is deleted from records.json

--- scraper/delete_data.py
+++ scraper/delete_data.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 import logging
+import pandas as pd
 
 from scraper.filemanager import Filemanager
 
@@ -39,19 +40,38 @@ def delete_from_record_data(
         record_data, category_names_to_delete, names_to_delete, ids_to_delete
     )
 
+    products_df = Filemanager.get_products_data()
+
     # Delete product ids
     for product_to_delete_id in products_to_delete_ids:
         category_name, product_name, website_name = product_to_delete_id
-        record_data[category_name][product_name].pop(website_name)
+        deleted_website_dict = record_data[category_name][product_name].pop(website_name)
+        url_to_delete = deleted_website_dict["info"]["url"]
+
+        # Delete row in products_df that match with the products that are deleted from record_data
+        products_df = delete_dataframe_rows(products_df, "short_url", url_to_delete)
 
     # Delete product names
     for product_to_delete_name in products_to_delete_names:
         category_name, product_name = product_to_delete_name
-        record_data[category_name].pop(product_name)
+        deleted_product_dict = record_data[category_name].pop(product_name)
+
+        # Delete row in products_df that match with the products that are deleted from record_data
+        for deleted_website_dict in deleted_product_dict.values():
+            url_to_delete = deleted_website_dict["info"]["url"]
+            products_df = delete_dataframe_rows(products_df, "short_url", url_to_delete)
 
     # Delete categories
     for category_to_delete in categories_to_delete:
-        record_data.pop(category_to_delete)
+        deleted_category_dict = record_data.pop(category_to_delete)
+
+        # Delete row in products_df that match with the products that are deleted from record_data
+        for deleted_product_dict in deleted_category_dict.values():
+            for deleted_website_dict in deleted_product_dict.values():
+                url_to_delete = deleted_website_dict["info"]["url"]
+                products_df = delete_dataframe_rows(products_df, "short_url", url_to_delete)
+
+    Filemanager.save_products_data(products_df)
 
 
 def get_categories_products_ids_to_delete(
@@ -75,3 +95,11 @@ def get_categories_products_ids_to_delete(
                     products_to_delete_ids.append((category_name, product_name, website_name))
 
     return categories_to_delete, products_to_delete_names, products_to_delete_ids
+
+
+def delete_dataframe_rows(products_df: pd.DataFrame, df_search_column: str, delete_value: str) -> pd.DataFrame:
+    # get the indexes to keep (not delete)
+    indexes_to_keep = products_df.index[products_df[df_search_column] != delete_value].tolist()
+    # get new dataframe with only the indexes to keep
+    new_products_df = products_df.loc[indexes_to_keep]
+    return new_products_df
