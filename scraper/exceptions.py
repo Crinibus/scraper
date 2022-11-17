fcf8e5c01fed9342818f0201742581a2c14266c0
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-11-17 21:15:17 +0100

    Add exception URLMissingSchema and constant URL_SCHEMES

--- scraper/exceptions.py
+++ scraper/exceptions.py
@@ -1,3 +1,6 @@
+from scraper.constants import URL_SCHEMES
+
+
 class WebsiteNotSupported(Exception):
     def __init__(self, website_name: str, *args: object) -> None:
         super().__init__(*args)
@@ -5,3 +8,12 @@ class WebsiteNotSupported(Exception):
 
     def __str__(self) -> str:
         return f"Website '{self.website_name}' is currently not supported"
+
+
+class URLMissingSchema(Exception):
+    def __init__(self, url, *args: object) -> None:
+        super().__init__(*args)
+        self.url = url
+
+    def __str__(self) -> str:
+        return f"Missing schema in url '{self.url}'. Consider prefixing the url with one of following schemes: {', '.join(URL_SCHEMES)}"
