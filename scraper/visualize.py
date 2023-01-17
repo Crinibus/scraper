5ec0848fb24dd70b651f2199c0aa54ec8acce5b8
Crinibus <57172157+Crinibus@users.noreply.github.com>
2023-01-18 00:59:18 +0100

    Revert "Move function get_master_products from visualize.py to new file get_products.py"
    This reverts commit 21b81161ffe0f04dc87eab8473c31fc7241f17a5.

--- scraper/visualize.py
+++ scraper/visualize.py
@@ -142,6 +142,25 @@ def show_products(products: list[Product], title: str) -> None:
     fig.show()
 
 
+def get_master_products(records_data: dict) -> tuple[MasterProduct]:
+    master_products: list[MasterProduct] = []
+
+    for category_name, category_info in records_data.items():
+        for product_name, product_info in category_info.items():
+            master_product = MasterProduct(product_name, category_name)
+            for website_name, website_info in product_info.items():
+                id = website_info["info"]["id"]
+                url = website_info["info"]["url"]
+                currency = website_info["info"]["currency"]
+                datapoints = [Datapoint(datapoint["date"], datapoint["price"]) for datapoint in website_info["datapoints"]]
+                is_up_to_date = is_datapoints_up_to_date(datapoints)
+                product = Product(product_name, category_name, url, id, currency, website_name, datapoints, is_up_to_date)
+                master_product.products.append(product)
+            master_products.append(master_product)
+
+    return tuple(master_products)
+
+
 def get_products_with_ids(master_products: tuple[MasterProduct], ids: list[str], only_up_to_date: bool) -> Iterator[Product]:
     for master_product in master_products:
         for product in master_product.products:
