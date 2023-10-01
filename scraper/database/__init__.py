dcf61db86ca5a0b5210d44efdcf272b4d8bd673d
Crinibus <57172157+Crinibus@users.noreply.github.com>
2023-10-01 18:06:20 +0200

    Add database functions - 'get_all_products', 'get_all_datapoints', 'get_products_by_categories' and 'get_products_by_names'

--- scraper/database/__init__.py
+++ scraper/database/__init__.py
@@ -1,4 +1,14 @@
 from .models import Product, DataPoint
 from .db import create_db_and_tables, engine
 
-from .functions import delete_all, get_product_by_product_code, get_products_by_product_codes, add_datapoint, add_product
+from .functions import (
+    delete_all,
+    get_all_products,
+    get_all_datapoints,
+    get_product_by_product_code,
+    get_products_by_product_codes,
+    get_products_by_categories,
+    get_products_by_names,
+    add_datapoint,
+    add_product,
+)
--- scraper/database/functions.py
+++ scraper/database/functions.py
@@ -10,6 +10,16 @@ def delete_all(elements: list[Product | DataPoint]) -> None:
         session.commit()
 
 
+def get_all_products() -> list[Product]:
+    with Session(engine) as session:
+        return session.exec(select(Product)).all()
+
+
+def get_all_datapoints() -> list[DataPoint]:
+    with Session(engine) as session:
+        return session.exec(select(DataPoint)).all()
+
+
 def get_product_by_product_code(product_code: str) -> Product | None:
     with Session(engine) as session:
         return session.exec(select(Product).where(Product.product_code == product_code)).first()
@@ -18,6 +28,16 @@ def get_product_by_product_code(product_code: str) -> Product | None:
 def get_products_by_product_codes(product_codes: list[str]) -> list[Product]:
     with Session(engine) as session:
         return session.exec(select(col(Product.product_code).in_(product_codes))).all()
+
+
+def get_products_by_categories(categories: list[str]) -> list[Product]:
+    with Session(engine) as session:
+        return session.exec(select(col(Product.category).in_(categories))).all()
+
+
+def get_products_by_names(names: list[str]) -> list[Product]:
+    with Session(engine) as session:
+        return session.exec(select(col(Product.name).in_(names))).all()
 
 
 def add_product(product: Product) -> None:
