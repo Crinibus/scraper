bf7179e852e4627539cace3ac4c16cf97ff87076
Crinibus <57172157+Crinibus@users.noreply.github.com>
2023-10-01 18:31:03 +0200

    Reorder import from '.functions' in database/__init__.py

--- scraper/database/__init__.py
+++ scraper/database/__init__.py
@@ -3,12 +3,12 @@ from .db import create_db_and_tables, engine
 
 from .functions import (
     delete_all,
+    add_datapoint,
+    add_product,
     get_all_products,
     get_all_datapoints,
     get_product_by_product_code,
     get_products_by_product_codes,
     get_products_by_categories,
     get_products_by_names,
-    add_datapoint,
-    add_product,
 )
