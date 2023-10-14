786f6a929de7c8d1996b6b6b5a810e9283c17079
Crinibus <57172157+Crinibus@users.noreply.github.com>
2023-10-14 13:09:38 +0200

    Delete misc.py
    Not necessary anymore because of database

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -8,7 +8,6 @@ from .delete_data import delete
 from .reset_data import reset
 from .search_data import search
 from .print_products import print_latest_datapoints, print_all_products
-from .misc import get_products_df_grouped_by_domains, get_products_grouped_by_domain
 from .format import Format
 import scraper.database as db
 
