f5ce7c76b38690ff004938c8ad564d5df18af140
Crinibus <57172157+Crinibus@users.noreply.github.com>
2023-11-09 23:13:11 +0100

    Delete function 'get_product_data'
    Not used anymore with the new database

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -18,18 +18,6 @@ class Scraper:
         self.product_info = self.website_handler.get_product_info()
 
 
-def get_product_data(data: dict, category: str, name: str, website_name: str) -> dict:
-    try:
-        product_data = data[category][name][website_name]
-        return product_data
-    except KeyError:
-        logging.getLogger(__name__).exception(
-            f"KeyError on dict 'data' with category: '{category}', name: '{name}' and website_name: '{website_name}'"
-        )
-        print("ERROR - KeyError - check logfile.log")
-        return None
-
-
 def start_threads_sequentially(threads: list[threading.Thread], request_delay: int, progress_bar=None) -> None:
     for thread in threads:
         thread.start()
