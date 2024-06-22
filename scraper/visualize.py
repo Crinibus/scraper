10aa6f7fd2dc03289d28fa6f3d181ec1e3670f00
sharifulgeo <msi_g@yahoo.com>
2024-06-22 12:00:36 +0600

    update congig_figure funciton.

--- scraper/visualize.py
+++ scraper/visualize.py
@@ -91,10 +91,7 @@ def show_products(products: list[ProductInfo], title: str) -> None:
             product,
             name_format="%website - %name - %id",
         )
-    title_ = dict(
-        text=title, font=dict(family="Courier New, monospace", color="#000000", size=18, weight="bold", style="italic")
-    )
-    config_figure(fig, title_)
+    config_figure(fig, title)
     fig.show(config={"scrollZoom": True})
 
 
@@ -163,7 +160,10 @@ def get_products_from_master_products(master_products: Iterable[MasterProduct]) 
 def config_figure(figure: go.Figure, figure_title: str) -> None:
     figure.update_traces(mode="markers+lines", hovertemplate=None)
     figure.update_layout(
-        title=figure_title,
+        title=dict(
+            text=figure_title,
+            font=dict(family="Courier New, monospace", color="#000000", size=18, weight="bold", style="italic"),
+        ),
         xaxis_title="Date",
         yaxis_title="Price",
         hovermode="closest",
